951d7f45c89e35dca7068d1d1995f94cba0306d3
mohammed-ibrahim <mohammed.ibrahim@practo.com>
2018-01-16 20:50:53 +0530

    Fix delegating sort

--- src/main/java/org/tools/csv/comparator/CsvComparator.java
+++ src/main/java/org/tools/csv/comparator/CsvComparator.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 import java.util.List;
 
 public class CsvComparator implements Comparator<String[]> {
-    
+
     private List<Integer> columnOrder = null;
 
     public CsvComparator(List<Integer> columnOrderItems) {
@@ -14,7 +14,7 @@ public class CsvComparator implements Comparator<String[]> {
 
     @Override
     public int compare(String[] o1, String[] o2) {
-        
+
         for (Integer columnNumber : this.columnOrder) {
 
             String originalLhs = cleanString(o1[columnNumber]);
@@ -22,46 +22,63 @@ public class CsvComparator implements Comparator<String[]> {
 
             Double lhs = safeDouble(originalLhs);
             Double rhs = safeDouble(originalRhs);
-            
+
             //Not a double match
             if (lhs == null && rhs == null) {
 
                 if (originalLhs == null && originalRhs == null) {
                     continue;
                 }
-                
+
                 if (originalLhs != null && originalRhs == null) {
                     return -1;
                 }
-                
+
                 if (originalLhs == null && originalRhs != null) {
                     return 1;
                 }
-                
-                return originalLhs.toLowerCase().compareTo(originalRhs.toLowerCase());
+
+                int result = originalLhs.toLowerCase().compareTo(originalRhs.toLowerCase());
+
+                if (result != 0) {
+
+                    return result;
+                } else {
+
+                    //delete to next row if current comparision is equal.
+                    continue;
+                }
             }
-            
+
             if (lhs != null || rhs != null) {
                 if (lhs != null && rhs == null) {
                     return -1;
                 }
-                
+
                 if (lhs == null && rhs != null) {
                     return 1;
                 }
-                
+
                 if (lhs.equals(rhs)) {
                     continue;
                 }
-                
-                return lhs.compareTo(rhs);
+
+                int result = lhs.compareTo(rhs);
+                if (result != 0) {
+
+                    return result;
+                } else {
+
+                    //delete to next row if current comparision is equal.
+                    continue;
+                }
             }
         }
-        
+
         //If all the columns are same then return 0;
         return 0;
     }
-    
+
     private String cleanString(String input) {
         if (input == null) {
             return null;
@@ -71,12 +88,12 @@ public class CsvComparator implements Comparator<String[]> {
         if (cleaned.isEmpty()) {
             return null;
         }
-        
+
         return cleaned;
     }
-    
+
     private Double safeDouble(String item) {
-        
+
         if (item == null) {
             return null;
         }
